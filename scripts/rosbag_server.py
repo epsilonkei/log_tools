811fdddd9893142f485aa6f3ae7dcd06e9b304a7
kindsenior <kunio.einstein@gmail.com>
2015-12-06 21:10:39 +0900

    add path deffinition and implement changing filename

--- scripts/rosbag_server.py
+++ scripts/rosbag_server.py
@@ -4,21 +4,40 @@
 # from log_tools.srv import *
 import log_tools.srv
 import rospy
-import time
+import rospkg
 
+import time
 import subprocess
+import os
+
+def create_file_name(filename):
+    localtime = time.localtime()
+    return filename + str(localtime.tm_year) + "-" + str(localtime.tm_mon) + "-" + str(localtime.tm_mday) + "-" + str(localtime.tm_hour) + "-" + str(localtime.tm_sec)
 
 def rosbag_command_callback(rosbag_command):
     rospy.loginfo("rosbag_command_callback()")
+    # start recording
     if rosbag_command.command == 'start':
-        localtime = time.localtime()
-        global filename = rosbag_command.filename + str(localtime.tm_year) + "-" + str(localtime.tm_mon) + "-" + str(localtime.tm_mday) + "-" + str(localtime.tm_hour) + "-" + str(localtime.tm_sec)
+        global filename0
+        filename0 = create_file_name(rosbag_command.filename)
         rospy.loginfo("start recording rosbag")
-        p = subprocess.Popen(["rosbag","record","-a","-O",filename])
+        p = subprocess.Popen(["rosbag","record","-a","-O",filename0])
+    # stop recording and save log
     elif rosbag_command.command == 'stop':
         rospy.loginfo("stop recording rosbag")
         subprocess.Popen(["pkill","rosbag","-2"])
         subprocess.Popen(["pkill","record","-2"])
+        global filename1
+        if rosbag_command.filename != "":
+            filename1 = create_file_name(rosbag_command.filename)
+        else:
+            filename1 = filename0
+        # move bagfile
+        if rosbag_command.path != "":
+            os.system("mkdir -p " + rosbag_command.path)
+            while not os.path.exists(os.path.join(rospkg.get_ros_home(),filename0) + ".bag"):
+                time.sleep(0.001)
+            os.system("mv " + os.path.join(rospkg.get_ros_home(),filename0) + ".bag " + os.path.join(rosbag_command.path,filename1) + ".bag")
     else :
         rospy.loginfo("invalid command")
 
